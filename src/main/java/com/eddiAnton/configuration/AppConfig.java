67605d7a95162fe2baa2862516eb63237d7da829
Eduard Anton <anton.e@cdek.ru>
2025-08-02 20:22:25 +0700

    add_logging_through_aop

--- src/main/java/com/eddiAnton/configuration/AppConfig.java
+++ src/main/java/com/eddiAnton/configuration/AppConfig.java
@@ -2,8 +2,10 @@ package com.eddiAnton.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan("com.eddiAnton")
+@Import(AopConfig.class)
 public class AppConfig {
 }
